7328cef4e7e34a16eb7488632696ddc27d9dc41b
Juan Rios <mrdaniel19996@gmail.com>
2020-09-15 15:47:11 -0500

    [UPDATE] Initial Receipes Mock, pending for check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './App.scss'
-import foto from '../src/static/Dailycius-logo/SVG/dailycius-logo-app.svg';
 import Nav from './components/Nav'
+
+import Home from './pages/Home';
 
 const App = () => (
   <div>
     <Nav />
-    <h1 className='wow' >Hello, react</h1>
-    <img className='wow-img' src={foto} alt="here is an fan img"/>
+    <Home />
+    
   </div>
 );
 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Home() {
+
+return <div className="hero">    	
+
+<div className="hero__panel">
+	<p>Oven Roasted Chicken Breasts<br />suburban soap box</p>
+		<div>
+				<button className="hero__buttons-1"><h1>4</h1><br />Ingredients</button>
+		</div>
+		<div>
+				<button className="hero__buttons-1"><h1>300</h1><br />Calories</button>
+		</div>
+		<div>
+				<button className="hero__buttons-1"><h1>33</h1><br />Minutes</button>
+		</div>
+		<article className="hero__button">
+			<button className="hero__button--Add">ADD INGREDIENTS TO CART</button>
+		</article>
+		<div>
+			<button className="hero__buttons">200<br />Calories</button>
+		</div>
+		<div>
+			<button className="hero__buttons">SODIUM<br />155% DV</button>
+		</div>
+		<div>
+			<button className="hero__buttons">FAT<br />15% DV</button>
+		</div>
+		<div>
+			<button className="hero__buttons">PROTEIN<br />61% DV</button>
+		</div>
+		<div>
+			<button className="hero__buttons">CARBS<br />1% DV</button>
+		</div>
+		<div>
+			<button className="hero__buttons">CARBS<br />1% DV</button>
+		</div>
+	</div>
+
+	<figure className="hero__image">
+		<img src="./assets/image-hero.png" />
+	</figure>
+
+	</div>
+	
+
+}
+
+export default Home;
