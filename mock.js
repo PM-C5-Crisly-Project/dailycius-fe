8138f628e6fbe3a29ae959eba60df5530822b706
Juan Rios <mrdaniel19996@gmail.com>
2020-09-15 22:34:31 -0500

    [UPDATE] Mock cheked

--- mock.js
+++ mock.js
@@ -1,22 +1,32 @@
+const { text } = require("body-parser");
+
 [
   {
     "id":001,
-    "category":"breackfast",
+    "category": "_id" ,
     "name":"Peanut butter and jelly puffs",
     "description":"Delicius receipe for your family ",
     "popular":false,
-    "photos":['https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/glliD45sT2YTxTYtGrHX_0S9A0257_ljywnk.jpg',
-    'https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/dIRNpdUwRPa1gW1gN4NW_0S9A0236_nmtfab.jpg',
-    'https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/5YESLpIRwSI33gVG4Ay9_0S9A0242_zjrqfm.jpg'],
+    "photos":["https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/glliD45sT2YTxTYtGrHX_0S9A0257_ljywnk.jpg",
+    "https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/dIRNpdUwRPa1gW1gN4NW_0S9A0236_nmtfab.jpg",
+    "https://res.cloudinary.com/djcsouoma/image/upload/v1600200830/receipes/rec-1/5YESLpIRwSI33gVG4Ay9_0S9A0242_zjrqfm.jpg"],
     "direction":[
-      'Preheat oven to 375°F.',
-      'Roll out pizza dough on a lightly floured surface to 1/8 -inch thickness. Using a round cookie cutter or glass 3-inches in diameter, cut circles of dough (re-roll leftover dough).',
-      'Place 1/2 teaspoon each of peanut butter and jelly in the center of each circle – do not spread. Wrap dough around filling and pinch shut. Place seam side down on a greased cookie sheet. Brush tops lightly with water or an egg wash, (1 egg beaten with 1 tablespoon water). If desired, sprinkle cinnamon and sugar on top before baking.',
-      'Bake for 15 to 17 minutes until lightly browned. Serve warm. Let cool at least 10 minutes before serving; jelly gets extremely hot. To reheat, place in microwave oven on medium heat, about 5 seconds per puff.'
+      "1. Preheat oven to 375°F.",
+      "2. Roll out pizza dough on a lightly floured surface to 1/8 -inch thickness. Using a round cookie cutter or glass 3-inches in diameter, cut circles of dough (re-roll leftover dough).",
+      "3. Place 1/2 teaspoon each of peanut butter and jelly in the center of each circle – do not spread. Wrap dough around filling and pinch shut. Place seam side down on a greased cookie sheet. Brush tops lightly with water or an egg wash, (1 egg beaten with 1 tablespoon water). If desired, sprinkle cinnamon and sugar on top before baking.",
+      "4. Bake for 15 to 17 minutes until lightly browned. Serve warm. Let cool at least 10 minutes before serving; jelly gets extremely hot. To reheat, place in microwave oven on medium heat, about 5 seconds per puff."
     ],
-    "ingredients":"['1. (10 ounce) can refrigerated pizza dough', '1/4. cup peanut butter', '1/4. cup jelly', ]"
+    "ingredients":[
+      "1. (10 ounce) can refrigerated pizza dough",
+      "1/4. cup peanut butter",
+      "1/4. cup jelly"],
+    "price": 500,
+    "comments": [{"id": "text" }],
+    "qualification": [{"id":"number"}]
   },
 ]
 
 //AGREGAR COMENTARIOS
 //CALIFICACION
+
+// ORDEN DEL ARRAY PUEDE CAMBIAR?
