2ebda31c35f2a8ea85381ca313216ed1084d15a4
Juan Rios <mrdaniel19996@gmail.com>
2020-09-17 22:44:57 -0500

    [UPDATE]Mock Update 10 receipes

--- mock.js
+++ mock.js
@@ -165,6 +165,137 @@ const { text } = require("body-parser");
     "comments": [{"id": "text" }],
     "qualification": [{"id":"number"}]
   },
+  {
+    "id":007,
+    "category": "_id" ,
+    "name":"waffle Cone Fried Chicken",
+    "description":"Delicius receipe for your family ",
+    "popular":false,
+    "photos":["https://res.cloudinary.com/djcsouoma/image/upload/v1600399097/receipes/rec-7/rec7-1_nqh6ya.jpg"],
+    "direction":[
+      "1. Preheat oil in a heavy bottomed pot to 350 degrees.",
+      "2. Combine the chicken and milk in bowl. In another bowl whisk together the flour, salt and pepper. Coat the chicken in the flour mixture.",
+      "3. Working in batches fry the chicken until golden brown and fully cooked through, about 4-5 minutes.",      
+      "4. Remove to a paper towel lined plate and season with salt.",      
+      "5. To assemble cones, fill waffle cones with chicken, drizzle with cheese sauce and garnish with chives.",      
+      "6. Cheese sauce: Bring the half and half to a simmer in a saucepan over medium heat. Add the cheese in batches and whisk until smooth. Serve immediately.",      
+    ],
+    "ingredients":[
+      "2. lbs chicken breasts, boneless skinless cut into 1 inch pieces",
+      "1. cup milk",
+      "1 1/2. cups flour",
+      "1. teaspoon kosher salt",
+      "1. teaspoon black pepper",
+      "1. cup canola oil (for frying)",
+      "1. bunch chives, thinly sliced",
+      "6. waffle cones, store bought",
+      "1/2. cup half-and-half",
+      "2. cups shredded yellow cheddar cheese",
+                
+    ],
+    "price": 110,
+    "comments": [{"id": "text" }],
+    "qualification": [{"id":"number"}]
+  },
+  {
+    "id":008,
+    "category": "_id",
+    "name":"Hamburgers",
+    "description":"Delicius receipe for your family ",
+    "popular":false,
+    "photos":["https://res.cloudinary.com/djcsouoma/image/upload/v1600399482/receipes/rec-8/rec8-2_g9moya.jpg"],
+    "direction":[
+      "1. Chop garlic cloves on wooden surface then put in mortar and pestle.",
+      "2. Add salt and olive oil and make a paste.",
+      "3. Add this to the meat along with the Worcestershire and horseradish.",      
+      "4. Mix everything and adjust ingredients to suit your taste.",      
+      "5. When you make the patties place an indention in the middle of each one with your thumb (almost all the way through). This keeps the burgers from shrinking. We like ours thick and I like them to stay that way!",      
+      "6. Salt and Pepper according to taste when done.",      
+      "7. An added note on 04-03-06 - Thank you all for your positive feedback. It's nice to get confirmation on something I thought was good and different.",      
+      "8. Please note that I usually add more ingredients than what is called for above. I nuke a tiny hamburger (meatball size) and taste. I usually add more of everything. The horseradish does not taste like horseradish when cooked. It gives it a flavor note that otherwise wouldn't be there. Unless you are allergic to it, please try it."
+    ],
+    "ingredients":[
+      "1. lb good quality lean ground beef",
+      "1. tablespoon olive oil",
+      "2. garlic cloves",
+      "1/4. teaspoon salt",
+      "3. tablespoons Worcestershire sauce",
+      "1. tablespoon horseradish (optional but good) (optional)"
+      
+                
+    ],
+    "price": 80,
+    "comments": [{"id": "text" }],
+    "qualification": [{"id":"number"}]
+  },
+  {
+    "id":009,
+    "category": "_id",
+    "name":"Yogurth Fruit Bars",
+    "description":"Delicius receipe for your family ",
+    "popular":false,
+    "photos":["https://res.cloudinary.com/djcsouoma/image/upload/v1600399878/receipes/rec-9/rec9-1_i1pl61.jpg"],
+    "direction":[
+      "1. Preheat oven to 350°F.",
+      "2. In a large bowl, mix all dry ingredients. Add dried fruits and walnuts.",
+      "3. In a medium bowl, combine yogurt, egg, apple juice concentrate, and oil.",      
+      "4. Make a well in the center of the dry ingredients and add the wet ingredients until blended.",      
+      "5. Coat a 9 X 13 inch pan with vegetable spray, spread batter in pan.",      
+      "6. Bake 45-50 minutes or until done. Cool 10 minutes before slicing.",      
+      "7. Slice into 8 servings. Freeze in individual portions if desired."
+      
+    ],
+    "ingredients":[
+      "1 1/2. cups whole wheat flour",
+      "1. teaspoon baking soda",
+      "1/2. teaspoon baking powder",
+      "1. teaspoon cinnamon",
+      "1/2. teaspoon ground ginger",
+      "1. (6 ounce) package mixed dried fruit, coarsely chopped",
+      "1/3. cup chopped walnuts (optional)",
+      "1. cup plain fat-free yogurt or 1 cup low-fat plain yogurt",
+      "1. large egg",
+      "1/4. cup apple juice concentrate",
+      "2. tablespoons oil",
+      "nonstick cooking spray"                
+    ],
+    "price": 250,
+    "comments": [{"id": "text" }],
+    "qualification": [{"id":"number"}]
+  },
+  {
+    "id":010,
+    "category": "_id",
+    "name":"Cheesy Taco Pockets",
+    "description":"Delicius receipe for your family ",
+    "popular":false,
+    "photos":["https://res.cloudinary.com/djcsouoma/image/upload/v1600400419/receipes/rec-10/rec10-2_qm9csh.jpg"],
+    "direction":[
+      "1. Remove pastry from freezer and allow to thaw at room temperature for about 30- 40 minutes or until they are easy to handle.",
+      "2. Meanwhile, add the beef, onion and green pepper to a large skillet and cook until beef is browned and vegetables begin to soften. Stir occasionally to break meat up and prevent scorching.",
+      "3. Add garlic and cook for one more minute, stirring constantly.",      
+      "4. Drain all the grease from the pan and return to the heat.",      
+      "5. Add tomato, catsup, cilantro or parsley, cumin, chili powder, oregano and cinnamon. Reduce heat to low and allow to simmer for about 10 minutes. You want a pretty dry mixture so your filling stays put in the pastry."
+      
+    ],
+    "ingredients":[
+      "2. (17 1/3 ounce) packages puff pastry sheets, 2 sheets per package",
+      "1. egg",
+      "1/2. tablespoon water",
+      "1 1/2. lbs ground beef",
+      "1/2. cup green pepper, chopped",
+      "1/2. cup white onion, chopped",
+      "1. garlic clove, minced",
+      "1. medium tomatoes, seeded and chopped",
+      "1/2. cup catsup",
+      "1/4. cup fresh cilantro or 1/4 cup parsley, chopped",
+      "1. teaspoon ground cumin",
+      "2. teaspoons chili powder"                
+    ],
+    "price": 310,
+    "comments": [{"id": "text" }],
+    "qualification": [{"id":"number"}]
+  },
 ]
 
 //AGREGAR COMENTARIOS
